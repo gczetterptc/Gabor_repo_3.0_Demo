942cc2d9baaf23c86ebe84a38bbdbb9507c3dff1
gczetterptc <gczetter@ptc.com>
2025-07-08 12:46:57 +0200

    #1020275 - new function added

--- GPIO_reader.java
+++ GPIO_reader.java
@@ -1,4 +1,27 @@
-GPIO Reader Code
-Changes
-More changes
-More
+import com.pi4j.io.gpio.*;
+import java.util.Random;
+
+public class RandomBlink {
+    public static void main(String[] args) throws InterruptedException {
+        // Create GPIO controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // Provision GPIO pin #01 as an output pin and turn it off
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+        pin.setShutdownOptions(true, PinState.LOW);
+
+        // Create random number generator
+        Random rand = new Random();
+
+        System.out.println("Random LED blinking. Press Ctrl+C to exit.");
+
+        // Blink LED randomly
+        while (true) {
+            pin.toggle(); // turn on or off
+            int delay = rand.nextInt(1000) + 200; // delay between 200ms to 1200ms
+            Thread.sleep(delay);
+        }
+
+        // gpio.shutdown();  // Normally would be here, but infinite loop for demo
+    }
+}
