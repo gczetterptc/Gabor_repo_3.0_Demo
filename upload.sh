42431b9ca11fb2d62a2c65e419f1bd560efaa65c
gczetterptc <gczetter@ptc.com>
2025-12-11 13:05:46 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@
 #
 log() {
     ts="$(date --iso-8601='seconds')"
-    echo -e "${ts} : $1" >> ${WORKSPACE}/target/upload.log"
+    echo -e "${ts} : $1" >> "${WORKSPACE}/target/upload.log"
 }
 LOG_FILENAME="${WORKSPACE}/target/upload.log"
 URL=$(echo ${JENKINS_URL} | awk -F":" '{print $1":"$2":9443/cb/rest/xunitresults/"}')
@@ -43,3 +43,4 @@ curl -v --location --request POST $URL -u "$USER:$PASS" \
     --form "file=@$FILE"\
 
 	>> ${LOG_FILENAME} 2>&1
+
