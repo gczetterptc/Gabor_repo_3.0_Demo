3c27b1837cee4bae9959d42ad14f8dc036c4a938
gczetterptc <gczetter@ptc.com>
2026-06-24 08:43:46 +0200

    #130591 - updates

--- Speed_sensore.java
+++ Speed_sensore.java
@@ -8,4 +8,8 @@ public class Example {
         int sum = addNumbers(5, 3);
         System.out.println("Sum: " + sum);
     }
+
+    public static boolean isEven(int number) {
+    return number % 2 == 0;
+}
 }
