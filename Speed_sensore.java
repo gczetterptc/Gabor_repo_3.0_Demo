f8caaa96326e10380ae7ba0c956f1a488e70b8d5
gczetterptc <gczetter@ptc.com>
2026-06-24 08:44:20 +0200

    #130589 - New class

--- Speed_sensore.java
+++ Speed_sensore.java
@@ -13,3 +13,17 @@ public class Example {
     return number % 2 == 0;
 }
 }
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void introduce() {
+        System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
+    }
+}
