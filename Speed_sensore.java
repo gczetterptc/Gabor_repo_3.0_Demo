8d663d86f6f62f41c78f590c67f9b5b875c13447
gczetterptc <gczetter@ptc.com>
2026-06-24 08:43:04 +0200

    #130593 - New function

--- Speed_sensore.java
+++ Speed_sensore.java
@@ -1,1 +1,11 @@
-New Code
+public class Example {
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = addNumbers(5, 3);
+        System.out.println("Sum: " + sum);
+    }
+}
